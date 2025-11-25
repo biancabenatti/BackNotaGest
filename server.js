a8a89f1906c64f31d763fb95188c36878e301d57
Bianca Pichirilo <bianca071995@gmail.com>
2025-11-25 09:42:19 -0300

    alteração cors

--- server.js
+++ server.js
@@ -13,7 +13,15 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://nota-gest.vercel.app'
+  ],
+  methods: ['GET','POST','PUT','DELETE'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Swagger
